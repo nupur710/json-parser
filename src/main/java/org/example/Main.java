8d82460cfb892d579c4fffb5900c9858d05f8207
Nupur Singh <nupur.singh2410@gmail.com>
2024-03-04 00:36:03 +0530

    Tokenize JSON input; rough

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,61 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/***
+ * Tokenize json string input. Worst case time complexity is
+ * O(n*2); needs to be improved
+ */
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc= new Scanner(System.in);
+        String userInput= sc.nextLine();
+        int constructNo= 0;
+        ArrayList<String> token= new ArrayList<>();
+        var builder= new StringBuilder();
+        var x= new StringBuilder();
+        char v;
+        char m= 0;
+        for(int i= 0; i<userInput.length(); i++) {
+            v= userInput.charAt(i);
+            if(userInput.charAt(i) == '{')
+                token.add(String.valueOf(userInput.charAt(i)));
+            if(userInput.charAt(i) == ':')
+                token.add(String.valueOf(userInput.charAt(i)));
+            if(userInput.charAt(i) == '"') {
+                token.add(String.valueOf(userInput.charAt(i)));
+                for(int j= i+1; j<userInput.length(); j++) {
+                    i= j;
+                    if(userInput.charAt(j) == '"') {
+                        m= userInput.charAt(j);
+                        break;
+                    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+                    x.append(userInput.charAt(j));
+                }
+                if(x!= null || !x.equals("")) {
+                token.add(String.valueOf(x));
+                x.setLength(0);
+                token.add(String.valueOf(m));
+            }
+            }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            if(Character.isDigit(userInput.charAt(i))) {
+                constructNo= constructNo*10 + Character.getNumericValue(userInput.charAt(i));
+                continue;
+            }
+            if(constructNo != 0) {
+                token.add(String.valueOf(constructNo));
+                constructNo = 0;
+            }
+
+            if(userInput.charAt(i) == '}') {
+                token.add(String.valueOf(userInput.charAt(i)));
+            }
         }
+        System.out.println(token);
     }
+
+
 }
