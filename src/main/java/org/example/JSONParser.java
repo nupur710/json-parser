8423bd17799b6cf4d77e5ea6057809a2971efd31
Nupur Singh <nupur.singh2410@gmail.com>
2024-04-02 22:13:51 +0530

    JSON Parser

--- src/main/java/org/example/JSONParser.java
+++ src/main/java/org/example/JSONParser.java
@@ -1,6 +1,45 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JSONParser {
+
+    private String input;
+    private int position;
+    private Lexer lexer;
+
+    public JSONParser(String input, InputStreamReader inputStreamReader) {
+        this.input= input;
+        this.position= 0;
+        this.lexer= new Lexer(inputStreamReader);
+    }
+
+    public ASTNode parse() throws IOException {
+        return parseObject();
+    }
+
+    private ASTNode parseObject() throws IOException {
+        if(input.charAt(position) != '{') { //first character is not opening bracket
+            //throw exception
+        } position++;
+        Map<String, ASTNode> keyValue= new HashMap<>();
+        while(input.charAt(position) != '}') {
+            String key= lexer.constructString();
+            if(input.charAt(position) != ':') {
+                //throw error
+            } position++;
+            ASTNode value= parseValue();
+            keyValue.put(key, value);
+            if(input.charAt(position) == ',') {
+                position++;
+            }
+        }
+        position++;
+        return new ObjectNode(keyValue);
+    }
 
 
 }
