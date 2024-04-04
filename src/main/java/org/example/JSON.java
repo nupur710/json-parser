98ecb2948de10e8e2eab36cc7fa4eee574ed95f1
Nupur Singh <nupur.singh2410@gmail.com>
2024-04-04 23:48:45 +0530

    read input

--- src/main/java/org/example/JSON.java
+++ src/main/java/org/example/JSON.java
@@ -1,5 +1,13 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //We will run pico-cli from this class
 public class JSON {
+    public static void main(String[] args) throws IOException {
+        JSONParser parser= new JSONParser(new InputStreamReader(System.in));
+        parser.parse();
+        System.out.println("Successful parsing!");
+    }
 }
