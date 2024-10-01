a3d8498a8773b092ee5226c40623c4f4e9583989
Nupur Singh <nupur.singh2410@gmail.com>
2024-10-01 23:46:21 +0530

    fix XML formatting

--- src/main/java/org/jsonparser/converttoxml/ConvertToXML.java
+++ src/main/java/org/jsonparser/converttoxml/ConvertToXML.java
@@ -7,57 +7,130 @@ import java.util.Map;
 
 public class ConvertToXML {
     private StringBuilder xml= new StringBuilder();
-    public void exportToXML(Node json) {
+    private int i= 0;
+    public void exportToXml(Node json) {
         xml.append("<Object>");
         parseObjectNode(json);
-        xml.append("\n\r</Object>");
+        xml.append("\n</Object>");
         System.out.println(xml.toString());
     }
 
     private void buildXML(String key, Node value) {
         if(value instanceof StringNode) {
-            parseStringNode(key, value, true);
+            i++;
+            parseStringNode(key, value, true, 0);
+            i--;
         } else if(value instanceof NumberNode) {
-            parseNumberNode(key, value, true);
+            i++;
+            parseNumberNode(key, value, true, 0);
+            i--;
         } else if(value instanceof BooleanNode) {
+            i++;
             parseBooleanNode(key, value, true);
+            i--;
         } else if(value instanceof ArrayNode) {
+            i++;
             parseArrayNode(key, value);
-        } else if (value instanceof ObjectNode) {
-            appendObjStart(key);
+            i--;
+        } else if(value instanceof ObjectNode) {
+            i++;
+            appendObjStart(key, i);
+            i++;
             parseObjectNode(value);
-            appendObjEnd(key);
+            i--;
+            appendObjEnd(key, i);
+            i--;
         }
     }
 
-    private void parseStringNode(String key, Node value, boolean k) {
+    private void parseStringNode(String key, Node value, boolean k, int r) {
         if(k) {
-            xml.append("\n\t<" + key + ">" + "\n\t\t<String>"
-                    + value.getNode() + "</String>" + "\n\t</" + key + ">");
+            xml.append("\n");
+            appendTabCharacters(i);
+            xml.append("<"+key+">");
+            xml.append("\n");
+            appendTabCharacters(i+1);
+            xml.append("<String>" + value.getNode() + "</String>");
+            xml.append("\n");
+            appendTabCharacters(i);
+            xml.append("</"+key+">");
         } else {
-            xml.append("\n\t<String>"
-                    + value.getNode() + "</String>");
-        }
-    }
-
-    private void parseNumberNode(String key, Node value, boolean k) {
-        if(k) {
-            xml.append("\n\t<" + key + ">" + "\n\t\t<Number>"
-                    + value.getNode() + "</Number>" + "\n\t</" + key + ">");
-        } else {
-            xml.append("\n\t<Number>"
-                    + value.getNode() + "</Number>");
+            xml.append("\n");
+            appendTabCharacters(r);
+            xml.append("<String>" + value.getNode() + "</String>");
         }
     }
 
     private void parseBooleanNode(String key, Node value, boolean k) {
         if(k) {
-            xml.append("\n\t<" + key + ">" + "\n\t\t<Boolean>"
-                    + value.getNode() + "</Boolean>" + "\n\t</" + key + ">");
+            xml.append("\n");
+            appendTabCharacters(i);
+            xml.append("<"+key+">");
+            xml.append("\n");
+            appendTabCharacters(i+1);
+            xml.append("<Boolean>" + value.getNode() + "</Boolean>");
+            xml.append("\n");
+            appendTabCharacters(i);
+            xml.append("</"+key+">");
         } else {
-            xml.append("\n\t<Boolean>"
-                    + value.getNode() + "</Boolean>");
+            xml.append("\n");
+            appendTabCharacters(i);
+            xml.append("<Boolean>" + value.getNode() + "</Boolean>");
         }
+    }
+
+    private void parseNumberNode(String key, Node value, boolean k, int r) {
+        if(k) {
+            xml.append("\n");
+            appendTabCharacters(i);
+            xml.append("<"+key+">");
+            xml.append("\n");
+            appendTabCharacters(i+1);
+            xml.append("<Number>" + value.getNode() + "</Number>");
+            xml.append("\n");
+            appendTabCharacters(i);
+            xml.append("</"+key+">");
+        } else {
+            xml.append("\n");
+            appendTabCharacters(r);
+            xml.append("<Number>" + value.getNode() + "</Number>");
+        }
+    }
+
+    private void parseArrayNode(String key, Node value) {
+        int j= i;
+        xml.append("\n");
+        appendTabCharacters(j);
+        xml.append("<"+key+">");
+        xml.append("\n");
+        appendTabCharacters(j+1);
+        xml.append("<Array>");
+        j= i+1;
+        if(value instanceof ArrayNode) {
+            List<Node> valueList= (List<Node>) value.getNode();
+            if(valueList.get(0) instanceof  StringNode) {
+                for(Node node : valueList) {
+                    j++;
+                    parseStringNode(key, node, false, j);
+                    j--;
+                }
+            } else if(valueList.get(0) instanceof NumberNode) {
+                for(Node node : valueList) {
+                    j++;  //should this be i++? then i--
+                    parseNumberNode(key, node, false, j);
+                    j--;
+                }
+            }
+        }
+        xml.append("\n");
+        appendTabCharacters(j);
+        xml.append("</Array>");
+        xml.append("\n");
+        appendTabCharacters(j-1);
+        xml.append("<"+key+"/>");
+
+        xml.append("\n");
+        //appendTabCharacters();
     }
 
     private void parseObjectNode(Node json) {
@@ -65,31 +138,35 @@ public class ConvertToXML {
         for(Map.Entry<String, Node> entry : jsonMap.entrySet()) {
             String key= entry.getKey();
             Node value= entry.getValue();
+            //    i++; //remove
             buildXML(key, value);
         }
     }
 
-    private void parseArrayNode(String key, Node value) {
-        xml.append("\n\t<"+key+">"+"\n\t\t<Array>");
-        if (value instanceof ArrayNode) {
-            {
-                List<Node> valueList = (List<Node>) value.getNode();
-                if(valueList.get(0) instanceof StringNode) {
-                    for (Node node : valueList) {
-                        parseStringNode(key, node, false);
-                    }
-                } else if(valueList.get(0) instanceof NumberNode) {
-                    for (Node node : valueList) {
-                        parseNumberNode(key, node, false);
-                    }
-                }
-            }
-        } xml.append("\n\r</Array>"+"\n\t<"+key+"/>");
+    private void appendObjStart(String key, int j) {
+        xml.append("\n");
+        appendTabCharacters(j);
+        xml.append("<"+key+">");
+        xml.append("\n");
+        appendTabCharacters(j+1);
+        xml.append("<Object>");
+        xml.append("\n");
+//        appendTabCharacters(j+1);
+//        xml.append("</"+key+">");
     }
-    private void appendObjStart(String key) {
-        xml.append("\n\t<"+key+">\n\t\t<Object>");
+
+    private void appendObjEnd(String key, int j) {
+        xml.append("\n");
+        appendTabCharacters(j+1);
+        xml.append("</Object>");
+        xml.append("\n");
+        appendTabCharacters(j);
+        xml.append("</"+key+">");
     }
-    private void appendObjEnd(String key) {
-        xml.append("\n\t</Object"+">\n\t\t</"+key+">");
+
+    private void appendTabCharacters(int x) {
+        for(int j= 0; j< x; j++){ //remove equal to
+            xml.append("\t");
+        }
     }
 }
